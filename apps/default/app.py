15d8883fac138df9204e8cb776de8d22d434a56e
Gareth Budge <gbudge@gmail.com>
2024-07-12 21:57:46 +1000

    Updates

--- apps/default/app.py
+++ apps/default/app.py
@@ -5,13 +5,60 @@
 import os
 import socket
 import getpass
+import logging
+import json
 
-import modules.apphelper as apphelper
+class LogFormatter(logging.Formatter):
+    datefmt:str = None
+    mesgfmt:str = None
+    indent:int = None
+
+    def __init__(self, date_format:str=None, mesg_format:str=None, json_indent:int=None):
+        if not date_format: self.datefmt = '%Y-%m-%d %H:%M:%S'
+        
+        if not mesg_format in ('text', 'json', 'csv'):
+            self.mesgfmt = 'json'
+        else:
+            self.mesgfmt = mesg_format
+        
+        # Ensure the indent is between 0 and 10.
+        if json_indent is not None and json_indent >= 0 and json_indent <= 10:
+            self.indent = json_indent
+        else:
+            self.indent = 0
+    
+    def format(self, record):
+        if self.mesgfmt == 'text':
+            return f'{self.formatTime(record, self.datefmt)} [{record.levelname}] {record.getMessage()}'
+        elif self.mesgfmt == 'csv':
+            return f'{self.formatTime(record, self.datefmt)},{record.levelname},{record.getMessage()}'
+        elif self.mesgfmt == 'json':
+            if self.indent:
+                return json.dumps({
+                    'timestamp': self.formatTime(record, self.datefmt),
+                    'level': record.levelname,
+                    'message': record.getMessage()
+                }, indent=self.indent)
+            else:
+                return json.dumps({
+                    'timestamp': self.formatTime(record, self.datefmt),
+                    'level': record.levelname,
+                    'message': record.getMessage()
+                })
+
 
 def main():
-    ah = apphelper.AppHelper(log_format='text')
+    log_stream_handler: logging.StreamHandler
+    logger: logging.Logger
 
-    ah._logger.info("Hello from your friendly Python container app!")
+    log_stream_handler = logging.StreamHandler()
+    log_stream_handler.setFormatter(LogFormatter(mesg_format='text'))
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(log_stream_handler)
+
+    logger.info("Hello from your friendly Python container app!")
     
     # Hostname (usually the container ID)
     logger.info(f"Hostname: {socket.gethostname()}")
